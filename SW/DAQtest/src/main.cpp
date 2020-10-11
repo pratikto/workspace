ac9dd773388a2b92d9603ecf4346a92436b0ae3d
Prats <pratikto.sulthoni.h@mail.ugm.ac.id>
2020-10-12 00:27:30 +0700

    DAQtest software

---
+++ SW/DAQtest/src/main.cpp
@@ -0,0 +1,76 @@
+
+/*
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
+
+#include "platform.h"
+//#include "ff.h"
+
+int main()
+{
+//    init_platform();
+
+	int status 		= XST_SUCCESS;
+	uint32_t count0_high	= 0;
+	uint32_t count0_low 	= 0;
+	uint64_t count0			= 0;
+	u32 count1_high	= 0;
+	u32 count1_low 	= 0;
+	u64 count1		= 0;
+
+	//Setup the interrupt
+	status = interrupt_init(
+			ScuGic_cfg_ptr,
+			&ScuGic,
+			&timer_processor);
+
+	if(status != XST_SUCCESS){
+		print("Interrupt setup failed\n\r");
+		exit(-1);
+	}
+
+    do{
+//    	if(READY_0_trig){
+    	if(checkBit(*(baseaddr_DAQ+4), 1)){
+    		count0_low 	= *(baseaddr_DAQ+1);
+    		count0_high = *(baseaddr_DAQ+0);
+			count0  	= ((uint64_t) (count0_low << 32) & 0xffffffff00000000) | ((uint64_t) count0_low & 0x00000000ffffffff);
+    		xil_printf("counter0_high :  %10u\n", count0_high);
+    		xil_printf("counter0_low  :  %10u\n\r", count0_low);
+			xil_printf("counter0 :  %10u\n", count0);
+			READY_0_trig = 0;
+    	}
+
+
+
+    	if(READY_1_trig){
+    		count1_low 	= *(baseaddr_DAQ+3);
+			count1_high = *(baseaddr_DAQ+2);
+			count1  	= ((u64) (count1_high << 32) & 0xffffffff00000000) + count1_low;
+			xil_printf("counter1 :  %10u\n", count1);
+			READY_1_trig =0;
+    	}
+
+    	//check O_ARM on AXI register
+//    	if (checkBit(*(baseaddr_DAQ+4), 5))
+//    		xil_printf("O_ARM is 1\n");
+//    	else
+//    		xil_printf("O_ARM is 0\n");
+
+
+    }while(1);
+
+    cleanup_platform();
+    return 0;
+}
+
---
+++ SW/DAQtest/src/platform.h
@@ -0,0 +1,148 @@
+/******************************************************************************
+*
+* Copyright (C) 2008 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+#ifndef __PLATFORM_H_
+#define __PLATFORM_H_
+
+
+/*============================================================================================================================================================
+ * Include libraries
+ *============================================================================================================================================================
+ */
+/*
+ * Several BSP (and standard C) header files need to be included
+ */
+#include <stdio.h>
+#include "xil_printf.h"
+#include <stdlib.h> 		// Standard C functions, e.g. exit()
+#include <stdbool.h>		// Provides a Boolean data type for ANSI/ISO-C
+#include "xparameters.h" 	// Parameter definitions for processor peripherals
+#include "xscugic.h" 		// Processor interrupt controller device driver
+#include "xscutimer.h"		//private timer
+#include "xsdps.h"
+//#include "ff.h"
+#include "platform_config.h"
+#include "xil_cache.h"
+#include <string>
+#include "xbasic_types.h"
+#include "xil_io.h"
+#include "xil_exception.h"
+
+/*============================================================================================================================================================
+ * Parameter definitions
+ *============================================================================================================================================================
+ */
+
+/*
+ * PL-PS interrupt port ID definition
+ */
+#define O_ARM			61
+#define O_SEL			62
+#define	O_A0			63
+#define O_A1			64
+#define O_Z0			65
+#define O_Z1			66
+#define O_OVERFLOW_0	67
+#define O_OVERFLOW_1	68
+#define O_READY_0		84
+#define O_READY_1		85
+
+/*
+ * check bit macro
+ */
+#define checkBit(var,pos) ((var) & (1<<(pos)))
+
+/*
+ * ============================================================================================================================================================
+ * Variable definitions
+ * ============================================================================================================================================================
+ */
+
+static bool READY_0_trig = 0;
+static bool READY_1_trig = 0;
+
+
+/*
+ * Define variables for the HLS block and interrupt controller instance data.
+ * The variables will be passed to driver API calls as handles in the respective
+ * hardware.
+ */
+
+static XScuGic ScuGic;											//Interrupt Controller Instance
+static XScuGic_Config *ScuGic_cfg_ptr;
+static XScuTimer timer_processor;								//processor timer
+static Xuint32 *baseaddr_DAQ = (Xuint32 *)XPAR_DAQ_0_BASEADDR; 	//DAQ base address
+
+/*
+ * Define global variables to interface with the interrupt service routine (ISR).
+ */
+
+void init_platform();
+void cleanup_platform();
+
+/*
+ * ============================================================================================================================================================
+ * Function definitions
+ * ============================================================================================================================================================
+ */
+/*
+ * Define a routine to setup the PS interrupt handler and register the
+ * HLS peripheral’s ISR.
+ * This functions sets up the interrupt on the ARM.
+ */
+int interrupt_init(
+		XScuGic_Config 	*intr_cfg_ptr,
+		XScuGic 		*intr_ptr,			//Interrupt Controller Instance
+		XScuTimer 		*Timer_ARM_ptr);	//ARM Timer instance
+
+/*
+ * Define an interrupt service routine for O_ARM
+ */
+static void O_ARM_isr(void *CallBackRef);
+
+/*
+ * Define an interrupt service routine for O_READY_0
+ */
+static void O_READY_0_isr(void *CallBackRef);
+
+/*
+ * Define an interrupt service routine for O_READY_1
+ */
+static void O_READY_1_isr(void *CallBackRef);
+
+/*
+ * Define an interrupt service routine for processor timer
+ */
+static void timer_processor_isr(void *CallBackRef);
+
+#endif
+
