1803ddcff2628f106ce8b4c3ed4bd3d48a837e17
Prats <pratikto.sulthoni.h@mail.ugm.ac.id>
2020-10-21 20:30:47 +0700

    C synthesis

--- HLS/PhaseError/solution1/script.tcl
+++ HLS/PhaseError/solution1/script.tcl
@@ -4,12 +4,13 @@
 ## Copyright (C) 1986-2020 Xilinx, Inc. All Rights Reserved.
 ############################################################
 open_project PhaseError
+set_top PhaseError
 add_files PhaseError/sources/PhaseError.cpp
 add_files PhaseError/sources/PhaseError.h
 open_solution "solution1" -flow_target vivado
 set_part {xc7z020clg400-1}
 create_clock -period 10 -name default
-#source "./PhaseError/solution1/directives.tcl"
+source "./PhaseError/solution1/directives.tcl"
 #csim_design
 csynth_design
 #cosim_design
